34330aed62d3f948a6b229ca308511cb8c7637d2
Shelina Gobardhan <shelinagobardhan@gmail.com>
2015-11-20 18:28:39 +0100

    Create groupproject.js

---
+++ groupproject.js
@@ -0,0 +1,10 @@
+var find = function() {
+    var MOVIEFIND = prompt("Are you a male or female?").toUpperCase();
+        if (MOVIEFIND === 'MALE' || MOVIEFIND === 'FEMALE') {
+            prompt("Will you watch this movie alone?");
+        }
+};
+
+var random = function() {
+    
+}
