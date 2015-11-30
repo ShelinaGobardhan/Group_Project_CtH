96724db9a31b12b30476dc00d225f5d88b7e2e22
Shelina Gobardhan <shelinagobardhan@gmail.com>
2015-11-30 22:12:44 +0100

    Update groupproject.js

--- groupproject.js
+++ groupproject.js
@@ -1,10 +1,36 @@
-var find = function() {
-    var MOVIEFIND = prompt("Are you a male or female?").toUpperCase();
-        if (MOVIEFIND === 'MALE' || MOVIEFIND === 'FEMALE') {
-            prompt("Will you watch this movie alone?");
-        }
-};
 
-var random = function() {
+var next = function() {
     
 }
+
+var myRequest = new XMLHttpRequest();
+    var method = "GET";
+    var url = "https://api.themoviedb.org/3/discover/movie?api_key=c035796a5a47aa7f45bf51d005852512&page=2";
+           
+        myRequest.open(method, url);
+        myRequest.send();
+
+console.log(document);
+
+
+var dataParsed;
+var i = 0;
+var MovieArray = [];
+
+myRequest.onreadystatechange = function () {
+    console.log(myRequest.readyState);
+    if (myRequest.readyState === 4 ) {
+        console.log(myRequest.responseText);
+        var data = myRequest.response;
+        dataParsed = JSON.parse(data);
+    }
+       
+      for (i = 0; i < dataParsed.results.length; i++){
+            MovieArray.push(dataParsed.results[i].genre_ids[0]);
+            console.log(dataParsed.results[i].genre_ids[0]);
+            if (dataParsed.results[i].genre_ids[80]) {
+                MovieArray.push(dataParsed.results[i].title)
+            }
+        }
+};   
+
